a2e1e851d782d257bff9c2a882aeb06dfd77c836
عبدالرحمن السراقبي <86768045+Abdalrhmanal@users.noreply.github.com>
2022-12-12 21:00:07 +0300

    Update viewcar.php

--- viewcar.php
+++ viewcar.php
@@ -10,6 +10,7 @@ if(isset($_POST['sub'])){
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="loginall.css">
     <style>
@@ -18,16 +19,67 @@ if(isset($_POST['sub'])){
             background-attachment: fixed;
         }
     </style>
-    <title>view car</title>
 </head>
 <body>
-    <table>
+    <!--navbar-bagkruond-->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <a class="navbar-brand" href="https://www.audi1909.net/search/label/%D8%B5%D9%88%D8%B1%20%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA">cars 2023</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation"></button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="https://www.audi1909.net/search/label/%D8%B5%D9%88%D8%B1%20%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA">buying a car</a>
+                        <a class="dropdown-item" href="https://www.audi1909.net/search/label/%D8%B5%D9%88%D8%B1%20%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA">Car swap</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="http://localhost/mdad/Reception.php">sign out</a>
+                </li>
+            </ul>
+            <form class="d-flex my-2 my-lg-0">
+                <input class="form-control me-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    
+<div class="table-responsive">
+    <table >
         <tr>
             <th><div class="card-wrap">
                 <div class="card">
                     <div class="card-front">
                         <div class="card-logo">
-                           <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1287.jpg"/>
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/2%D8%AF-1.jpg?fit=899%2C599&ssl=1"/>
+                        </div>
+                    </div>
+            
+                    <div class="card-back">
+                        <div class="user">
+                            <img src="https://1.bp.blogspot.com/-OITDRrWBGiQ/YUZGU-2KxPI/AAAAAAACRow/w49RlEXE9o4-4Yv277TIosEaQFb83qKFACLcBGAsYHQ/w400-h225/86.jpg" alt="user-profile-picture">
+                        </div>
+                        <div class="user-info">
+                            <div class="username">Project overview</div>
+                            <div class="user-bio">
+                                <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>
+                            </div>
+                            <button class="follow-btn" onclick="window.location.href='https://www.audi1909.net/2021/02/Paris-Motor-Show.html'">information</button>
+                        </div>
+                    </div>
+                </div>
+                </div></th>
+            </th>
+            <th><div class="card-wrap">
+                <div class="card">
+                    <div class="card-front">
+                        <div class="card-logo">
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/33-4.jpg?resize=696%2C465&ssl=1"/>
                         </div>
                     </div>
             
@@ -38,8 +90,7 @@ if(isset($_POST['sub'])){
                         <div class="user-info">
                             <div class="username">Project overview</div>
                             <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
-                         
+                           <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>                         
                             </div>
                             <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
                         </div>
@@ -51,7 +102,7 @@ if(isset($_POST['sub'])){
                 <div class="card">
                     <div class="card-front">
                         <div class="card-logo">
-                           <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1287.jpg"/>
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/4-1.jpg?resize=696%2C372&ssl=1"/>
                         </div>
                     </div>
             
@@ -62,31 +113,7 @@ if(isset($_POST['sub'])){
                         <div class="user-info">
                             <div class="username">Project overview</div>
                             <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
-                         
-                            </div>
-                            <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
-                        </div>
-                    </div>
-                </div>
-                </div></th>
-            </th>
-            <th><div class="card-wrap">
-                <div class="card">
-                    <div class="card-front">
-                        <div class="card-logo">
-                           <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1287.jpg"/>
-                        </div>
-                    </div>
-            
-                    <div class="card-back">
-                        <div class="user">
-                            <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1288.jpg" alt="user-profile-picture">
-                        </div>
-                        <div class="user-info">
-                            <div class="username">Project overview</div>
-                            <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
+                             <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>
                          
                             </div>
                             <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
@@ -101,7 +128,7 @@ if(isset($_POST['sub'])){
                 <div class="card">
                     <div class="card-front">
                         <div class="card-logo">
-                           <img src="gg1.jpg"/>
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/5-1.jpg?resize=696%2C377&ssl=1"/>
                         </div>
                     </div>
             
@@ -112,7 +139,7 @@ if(isset($_POST['sub'])){
                         <div class="user-info">
                             <div class="username">Project overview</div>
                             <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
+                             <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>
                          
                             </div>
                             <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
@@ -125,7 +152,7 @@ if(isset($_POST['sub'])){
                 <div class="card">
                     <div class="card-front">
                         <div class="card-logo">
-                           <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1287.jpg"/>
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/66-1.jpg?resize=696%2C392&ssl=1"/>
                         </div>
                     </div>
             
@@ -136,7 +163,7 @@ if(isset($_POST['sub'])){
                         <div class="user-info">
                             <div class="username">Project overview</div>
                             <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
+                             <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>
                          
                             </div>
                             <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
@@ -149,7 +176,7 @@ if(isset($_POST['sub'])){
                 <div class="card">
                     <div class="card-front">
                         <div class="card-logo">
-                           <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1287.jpg"/>
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/7.jpg?resize=696%2C391&ssl=1"/>
                         </div>
                     </div>
             
@@ -160,7 +187,7 @@ if(isset($_POST['sub'])){
                         <div class="user-info">
                             <div class="username">Project overview</div>
                             <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
+                             <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>
                          
                             </div>
                             <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
@@ -174,7 +201,7 @@ if(isset($_POST['sub'])){
                 <div class="card">
                     <div class="card-front">
                         <div class="card-logo">
-                           <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1287.jpg"/>
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/9.jpg?resize=696%2C521&ssl=1"/>
                         </div>
                     </div>
             
@@ -185,7 +212,7 @@ if(isset($_POST['sub'])){
                         <div class="user-info">
                             <div class="username">Project overview</div>
                             <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
+                             <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>
                          
                             </div>
                             <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
@@ -198,7 +225,7 @@ if(isset($_POST['sub'])){
                 <div class="card">
                     <div class="card-front">
                         <div class="card-logo">
-                           <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1287.jpg"/>
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/10.jpg?resize=663%2C420&ssl=1"/>
                         </div>
                     </div>
             
@@ -209,7 +236,7 @@ if(isset($_POST['sub'])){
                         <div class="user-info">
                             <div class="username">Project overview</div>
                             <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
+                            <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>
                          
                             </div>
                             <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
@@ -222,7 +249,7 @@ if(isset($_POST['sub'])){
                 <div class="card">
                     <div class="card-front">
                         <div class="card-logo">
-                           <img src="https://3tab.net/wp-content/uploads/2018/03/unnamed-file-1287.jpg"/>
+                           <img src="https://i0.wp.com/yesiraq.com/wp-content/uploads/2020/06/1-2.jpg?resize=696%2C391&ssl=1"/>
                         </div>
                     </div>
             
@@ -233,7 +260,7 @@ if(isset($_POST['sub'])){
                         <div class="user-info">
                             <div class="username">Project overview</div>
                             <div class="user-bio">
-                                <p><h4><i>It is a prison system management project</i></h4>
+                             <h6><i>Modern car showroom project !!Note: This project is under modification</i></h6>
                          
                             </div>
                             <button class="follow-btn" onclick="window.location.href='https://m.facebook.com/story.php?story_fbid=pfbid0a6xmzRtxn87yowSdcGkkkwHVsr4CPvnCZw8nQHELY4qrKWJyd8y2JpraqCp5MH17l&id=100060889084046'">information</button>
@@ -241,9 +268,10 @@ if(isset($_POST['sub'])){
                     </div>
                 </div>
                 </div></th>
-            </th>
+
         </tr>
     </table>
-
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
